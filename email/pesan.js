5bea8c6ea3872a4822b0e1339270c42ac655c56b
fr34ks <donisanjaya9798@gmail.com>
2022-10-18 19:53:01 +0700

    geekzbotv6

---
+++ email/pesan.js
@@ -0,0 +1,28 @@
+// virtex lu
+const gmail1 = `
+Hay Fitur error yang anda gunakan sudah di Fix oleh moderator Geekz BOT
+Jika ada fitur error lainya ketik .report ya
+Agar kami tangani secepatnya 
+
+Thank you for using our Bot ☺️
+
+const gmail = pickRandom([gmail1])
+
+export default virtex;
+
+function pickRandom(list) {
+    return list[Math.floor(list.length * Math.random())]
+}
+
+
+
+/**
+ * Jangan Di Hapus!!
+ * 
+ * Buatan @FokusDotId (Fokus ID)
+ * Github: https://github.com/fokusdotid
+ * 
+ * Ingin bikin fitur tapi tidak bisa coding?
+ * hubungi: https://wa.me/6281320170984
+ * 
+ */
